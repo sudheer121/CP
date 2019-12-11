5d34af596b6568fb9f17acf6beb052c533a700a5
Sudheer Tripathi <31629433+Sudheer121@users.noreply.github.com>
2019-12-11 16:49:57 +0530

    Update long.cpp

--- long.cpp
+++ long.cpp
@@ -1,45 +1,85 @@
-//prob 1
+//suffix array
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long 
-int a[9]; 
-int main() {
-int t,n,x,y; ll sum;
-cin>>t;
-while(t--)
+string s;
+int suffixarr[100100];//contains 
+struct sfx
 {
-cin>>n;sum=0;
-memset(a,0,9*sizeof(int));
-while(n--)
+  int idx; int r1; int r2;
+};
+bool cmp(sfx a,sfx b)
 {
-  cin>>x>>y;
-  if(x<9 && y>a[x]) { a[x]= y; }
+  if(a.r1 != b.r1) { return a.r1<b.r1;}
+  else return a.r2<b.r2;
 }
-for(int i=1;i<9;i++) { sum+=a[i]; }
-cout<<sum<<endl;
-}
-return 0;
-}
-//prob2
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long 
-int t,n,a[3];
-ll x,ans;
-int main()
+void sfxarr(string s,int n)
 {
-cin>>t;
-while(t--)
-{
-cin>>n;a[0]=0;a[2]=0;ans=0;
+sfx arr[n];
 for(int i=0;i<n;i++)
 {
-  cin>>x;
-  if(x==0) { a[0]++; }
-  if(x==2) { a[2]++; }
-}  
-ans = (a[0]*(a[0]-1))/2 + (a[2]*(a[2]-1))/2;
-cout<<ans<<endl;
-}  
-  return 0;
+  arr[i].idx = i;
+  arr[i].r1 = s[i]-'a';
+  if(i==n-1) { arr[i].r2 = -1;continue;}
+  arr[i].r2 = s[i+1] - 'a';
 }
+sort(arr, arr+n, cmp); 
+int ind[n];// idx to position mapping
+for(int k=4;k<=2*n;k*=2)
+{
+int new_rank = 0;
+int old_rank = arr[0].r1; 
+arr[0].r1 = new_rank;
+ind[arr[0].idx] = 0; 
+for(int i=1;i<n;i++)//assign r1
+{
+if(arr[i].r1 == old_rank && arr[i].r2 == arr[i-1].r2)
+{
+old_rank = arr[i].r1;
+arr[i].r1 = new_rank;
+} 
+else
+{
+old_rank = arr[i].r1;
+arr[i].r1 = ++new_rank;
+}
+ind[arr[i].idx] = i;
+}
+for(int i=0;i<n;i++)
+{
+int next_idx = arr[i].idx + k/2;
+if(next_idx >= n) {arr[i].r2 = -1; continue;}
+arr[i].r2 = arr[ind[next_idx]].r1;
+}
+sort(arr, arr+n, cmp); 
+}//nlognlogn
+for(int i=0;i<n;i++) { suffixarr[i] = arr[i].idx; }
+}
+
+int  lcpfunc(int n)
+{
+int lcp = 0; int x,y,temp;
+for(int i=1;i<n;i++)
+{ temp = 0;
+x = suffixarr[i];
+y = suffixarr[i-1];
+while(x<n && y<n && s[x]==s[y]) 
+{
+temp++;x++;y++;
+}
+if(temp>lcp) { lcp=temp; }
+}//for  
+return lcp;
+}
+
+int main()
+{int t;cin>>t;
+while(t--)
+{ int temp;
+cin>>temp>>s;  
+sfxarr(s,s.size());
+//for(int i=0;i<s.size();i++){ cout<<suffixarr[i]<<" ";}
+cout<<lcpfunc(s.size())<<endl;
+}
+return 0;}
+
+
