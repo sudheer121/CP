7aca66a081f5dfe60e8f965ede173a31e9c1776f
Sudheer Tripathi <31629433+Sudheer121@users.noreply.github.com>
2020-05-15 20:37:06 +0530

    Update Kadane's algo.cpp

--- DP/max subarray sum/Kadane's algo.cpp
+++ DP/max subarray sum/Kadane's algo.cpp
@@ -7,61 +7,51 @@ Let b[] be an array where b[i] is the maximum sum ending at i.
 Let 'ans' store the maximum value calculated on the way. -> max so far.
 Let's say I have calculated the answer till b[i-1]
 
-b[i] = max(b[i-1]+a[i],a[i])
+We can say,
+if(a[i] > b[i-1] + a[i]):
+    b[i] = a[i]
+else 
+    b[i] = b[i-1] + a[i]
+    
+In short, b[i] = max(b[i-1]+a[i],a[i])
 Maximum sum ending at index i either continues max sum ending at i-1 or is on its own.
 Then ans = max(ans,b[i])
 
 */
-
-#include<bits/stdc++.h>
-using namespace std;
-const int maxn = 1e5 + 10;
-
-int n,ans=0;
-int a[maxn],b[maxn]; 
-
-int main()
+void soln1(int a[],int n)
 {
-cin>>n;
-for(int i=0;i<n;i++) { cin>>a[i]; }
+int ans=0;
+int b[maxn];     
 b[0] = a[0]; 
 for(int i=1;i<n;i++)
 {
 b[i] = max(b[i-1]+a[i],a[i]); 
 ans = max(ans,b[i]);
 }
-cout<<ans; 
-return 0;
+return ans;     
 }
+
 /*
-If u notice closely, we can also solve the answer without considering the extra array b[]
-Let's call b[i] as max_ending_here
+We don't need array b[] because at any point we only need b[i-1].
+Let's store b[i-1] in max_ending_here and update it in every iteration. 
 
 Consider u are at index i, so max_ending_here has the solution till i-1;
-if (max_ending_here + a[i] > max_ending_here):
-    max_ending_here = a[i] + max_ending_here
-else:
+if (a[i] > max_ending_here + a[i]):
     max_ending_here = a[i] 
+else:
+    max_ending_here = a[i] + max_ending_here
+
+Inshort, max_ending_here = max(a[i], max_ending_here + a[i]);
 ans = max(ans,max_ending_here)
 
-This approach is coded below.
-max_current = max_ending_here
 */
 
-
-
-//KADANE'S ALGORITHM 
-#include<bits/stdc++.h>
-using namespace  std;
-#define ll long long
-const int maxn = 1e5 + 10;
-int n,a[maxn];
-
-int kadane(int a[],int n)
+int soln2(int a[],int n)
 {
+// Here, ans is maxglobal and max_ending_here is maxcurrent
 int maxcurrent,maxglobal;
-maxcurrent = maxglobal = a[1];
-for(int i=2;i<=n;i++)
+maxcurrent = maxglobal = a[0];
+for(int i=1;i<n;i++)
 {
 maxcurrent = max(maxcurrent+a[i],a[i]);
 if(maxcurrent>maxglobal) {  maxglobal = maxcurrent; }
@@ -69,12 +59,3 @@ if(maxcurrent>maxglobal) {  maxglobal = maxcurrent; }
 return maxglobal;
 }
 
-int main()
-{
-cin>>n;
-for(int i=1;i<=n;i++)
-{
-  cin>>a[i];
-}
-cout<<kadane(a,n);
-return 0;}
