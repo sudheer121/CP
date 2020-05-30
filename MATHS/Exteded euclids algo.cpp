bfb116060c370562f2c661aa0df5cfbac58426b2
Sudheer Tripathi <31629433+Sudheer121@users.noreply.github.com>
2020-05-30 21:55:51 +0530

    Update Exteded euclids algo.cpp

--- MATHS/Exteded euclids algo.cpp
+++ MATHS/Exteded euclids algo.cpp
@@ -1,17 +1,17 @@
 //Extended EUCLID'S algorithm gives gcd in terms of original numbers
 #include<bits/stdc++.h>
 using namespace std;
-int x_1,y_1,gcd,temp;//x_1 is x1,y_1 is y_1
+int gx,gy,gcd,temp;//x_1 is x1,y_1 is y_1
 int GCD(int A, int B) {
     
     if(B==0)
-        {   x_1=1;y_1=0;gcd=A;
+        {   gx=1;gy=0;gcd=A;
             return A;
         }
     else
         {   GCD(B, A % B);
-            temp=x_1;
-            x_1=y_1; y_1=temp-(A/B)*y_1;
+            temp = x_1;
+            gx = gy; gx = temp-(A/B)*gy;
             return 0;
         }
 }
