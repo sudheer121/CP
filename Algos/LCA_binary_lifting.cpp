9df9bf5d2049c414bf207b77cc2906d454952c93
Sudheer Tripathi <31629433+Sudheer121@users.noreply.github.com>
2020-07-19 10:40:41 +0530

    Update LCA_binary_lifting.cpp

--- Algos/LCA_binary_lifting.cpp
+++ Algos/LCA_binary_lifting.cpp
@@ -51,6 +51,14 @@ void preprocess(int root) {
 /*
 ===============================================================================================================================================================================
 Explanation of for loop inside lca() function.
-We start climbing up from one side of 2 vertices,eg:u until  we reach the lca.
-Imagine the distance of u from lca is a binary number.eg:  
+We start climbing up from one side of 2(u,v) vertices,eg:u until  we reach the lca.
+Imagine the distance of u from lca is a number x, if x=13 then x in binary is 1101. 
+If n = 64, l = 6.
+Hence first we climb 2^6. 
+Check if 64th parent of u is not ancestor of v. Keep checking 32nd parent , 16th parent,8th parent of u and so on. 
+According to example 8th parent is not ancestor, so climb to the 8th parent. Then 4th parent from here is not an ancestor so climb to 4th parent.
+Now, 2nd parent from here is an ancestor,so skip. Now 1 parent above is a also lca , so skip. Loop ends here.Now just climb 1 parent and you get lca. 
+Basically we made use of binary representation of 13. We climbed accordingly from MSB to LSB.We climbed if MSB was one, and didn't if it was zero. 
+
+
 */
