0c00972a0bc0f54b353680bd81ca2c593f31ff0a
Sudheer Tripathi <31629433+Sudheer121@users.noreply.github.com>
2020-10-16 23:14:29 +0530

    Update detect_cycles.cpp

--- Graphs/detect_cycles.cpp
+++ Graphs/detect_cycles.cpp
@@ -35,6 +35,9 @@ bool checkCyclic() // call this function in main()
 
 //THEORY
 /*
-Cycle detection in undirected graphs is easy -->  just see if a node has been visited peviously.
+Cycle detection in undirected graphs is easy -->  just see if a node has been visited peviously.But this can't be done for DAG because it returns true in case of false cycle
+meaning it's closed but not completely cyclic eg: 1->2->3->4, 1->4 
+
 Cycle detection in DAG is done by using a recusion stack, the idea being if a cycle exists, we will encounter the repeated element within the same dfs branch. 
+Basically you check if the vertex is in recursion stack. 
 */
